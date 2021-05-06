960df269f42441bc5397853ee615c156a889c568
Anastasia Starodubtseva <anastasiast1605@gmail.com>
2021-05-06 23:23:33 +0300

    Implementing slides

--- main.js
+++ main.js
@@ -1,26 +1,89 @@
+function reducer(model, action) {
+  switch(action.type) {
+    case 'PREVIOUS-IMAGE':
+      if (model.currentIndex === 0) {
+        model.currentIndex = model.images.length - 1;
+      } else {
+        model.currentIndex -= 1;
+      }
+      return model;
+    case 'NEXT-IMAGE':
+      if (model.currentIndex === model.images.length -1) {
+        model.currentIndex = 0;
+      } else {
+        model.currentIndex +=1;
+      }
+      return model;
+    case 'FIRST-IMG-INDICATOR':
+      model.currentIndex = 0;
+      return model;
+    case 'SECOND-IMG-INDICATOR':
+      model.currentIndex = 1;
+    return model;
+    case 'THIRD-IMG-INDICATOR':
+      model.currentIndex = 2;
+    return model;
+    default:
+      return model;
+  }
+}
+
+var store = Redux.createStore(reducer, {
+  images: [
+    './img/img (15).jpeg',
+    './img/img (22).jpeg',
+    './img/img (23).jpeg'
+  ],
+  currentIndex: 0,
+})
+
 const e = React.createElement;
 
 function render() {
+
   ReactDOM.render (
-    e('div', {className: 'carousel'}, [
-      e('p', {className: 'header'}, 'gallery'),
-      e('div', {className: 'carousel-items'}, [
-        e('img', {className: 'first-item', src: './img/img (22).jpeg'}, null),
-        // e('img', {className: 'second-item', src: './img/img (15).jpeg'}, null),
-        // e('img', {className: 'third-item', src: './img/img (23).jpeg'}, null)
+    e(ReactRedux.Provider, {store: store}, [
+      e('div', {className: 'carousel-wrapper'}, [
+        e('div', {className: 'carousel'}, [
+          e('p', {className: 'header'}, 'gallery'),
+          e('div', {className: 'carousel-items'}, [
+            e('img', {className: 'carousel__photo initial', src: store.getState().images[store.getState().currentIndex]}, null),
+
+            e('div', {className: 'carousel-title'}, [
+              e('p', {className: 'title-one'}, 'First slide')
+            ]),
+            e('button', {className: 'carousel__button--prev', onClick: function(event) {
+              store.dispatch({type: 'PREVIOUS-IMAGE'})
+            }}, [ e('i', {className: 'fas fa-chevron-left'}, null)], null),
+
+            e('button', {className: 'carousel__button--next', onClick: function(event) {
+              store.dispatch({type: 'NEXT-IMAGE'})
+            }}, [ e('i', {className: 'fas fa-chevron-right'}, null)]),
+          ]),
+
+          e('div', {className: 'bottoms-flipping'}, [
+            e('button', {className: 'left-circle', onClick: function(event) {
+              store.dispatch({type: 'FIRST-IMG-INDICATOR'})
+            }}, [
+              e('i', {className: 'fas fa-circle'}, null)
+            ]),
+            e('button', {className: 'middle-circle', onClick: function(event) {
+              store.dispatch({type: 'SECOND-IMG-INDICATOR'})
+            }}, [
+              e('i', {className: 'fas fa-circle'}, null)
+            ]),
+            e('button', {className: 'right-circle', onClick: function(event) {
+              store.dispatch({type: 'THIRD-IMG-INDICATOR'})
+            }}, [
+              e('i', {className: 'fas fa-circle'}, null)
+            ])
+          ]),
+        ])
       ]),
-      e('button', {className: 'left-side'}, [
-        e('i', {className: 'fas fa-chevron-left'}, null)
-      ]),
-      e('button', {className: 'right-side'}, [
-        e('i', {className: 'fas fa-chevron-right'}, null)
-      ]),
-      e('p', {className: 'name-of-the-image'}, 'First slide'),
     ]),
     document.getElementById('root')
   )
-
-  document.querySelector('.carousel');
 }
 
+store.subscribe(function() { render(); });
 render();
