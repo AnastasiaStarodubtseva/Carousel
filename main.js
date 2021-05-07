e1bb675684ff03869b025a21f31e1f051721fd0e
Anastasia Starodubtseva <anastasiast1605@gmail.com>
2021-05-07 20:38:02 +0300

    Fix styles

--- main.js
+++ main.js
@@ -43,42 +43,37 @@ function render() {
 
   ReactDOM.render (
     e(ReactRedux.Provider, {store: store}, [
-      e('div', {className: 'carousel-wrapper'}, [
-        e('div', {className: 'carousel'}, [
-          e('p', {className: 'header'}, 'gallery'),
-          e('div', {className: 'carousel-items'}, [
-            e('img', {className: 'carousel__photo initial', src: store.getState().images[store.getState().currentIndex]}, null),
+      e('div', {className: 'carousel-items'}, [
+        e('p', {className: 'header'}, 'gallery'),
+        e('div', {className: 'img-section'}, [
+          e('img', {className: 'carousel__photo', src: store.getState().images[store.getState().currentIndex]}, null),
+          e('button', {className: 'carousel__button--prev', onClick: function(event) {
+            store.dispatch({type: 'PREVIOUS-IMAGE'})
+          }}, [ e('i', {className: 'fas fa-chevron-left'}, null)], null),
+          e('button', {className: 'carousel__button--next', onClick: function(event) {
+            store.dispatch({type: 'NEXT-IMAGE'})
+          }}, [ e('i', {className: 'fas fa-chevron-right'}, null)]),
+        ]),
+        e('p', {className: 'title-one'}, 'First slide'),
 
-            e('div', {className: 'carousel-title'}, [
-              e('p', {className: 'title-one'}, 'First slide')
-            ]),
-            e('button', {className: 'carousel__button--prev', onClick: function(event) {
-              store.dispatch({type: 'PREVIOUS-IMAGE'})
-            }}, [ e('i', {className: 'fas fa-chevron-left'}, null)], null),
 
-            e('button', {className: 'carousel__button--next', onClick: function(event) {
-              store.dispatch({type: 'NEXT-IMAGE'})
-            }}, [ e('i', {className: 'fas fa-chevron-right'}, null)]),
+        e('div', {className: 'bottoms-flipping'}, [
+          e('button', {className: 'indicator', onClick: function(event) {
+            store.dispatch({type: 'FIRST-IMG-INDICATOR'})
+          }}, [
+            e('i', {className: 'fas fa-circle'}, null)
           ]),
-
-          e('div', {className: 'bottoms-flipping'}, [
-            e('button', {className: 'left-circle', onClick: function(event) {
-              store.dispatch({type: 'FIRST-IMG-INDICATOR'})
-            }}, [
-              e('i', {className: 'fas fa-circle'}, null)
-            ]),
-            e('button', {className: 'middle-circle', onClick: function(event) {
-              store.dispatch({type: 'SECOND-IMG-INDICATOR'})
-            }}, [
-              e('i', {className: 'fas fa-circle'}, null)
-            ]),
-            e('button', {className: 'right-circle', onClick: function(event) {
-              store.dispatch({type: 'THIRD-IMG-INDICATOR'})
-            }}, [
-              e('i', {className: 'fas fa-circle'}, null)
-            ])
+          e('button', {className: 'indicator', onClick: function(event) {
+            store.dispatch({type: 'SECOND-IMG-INDICATOR'})
+          }}, [
+            e('i', {className: 'fas fa-circle'}, null)
           ]),
-        ])
+          e('button', {className: 'indicator', onClick: function(event) {
+            store.dispatch({type: 'THIRD-IMG-INDICATOR'})
+          }}, [
+            e('i', {className: 'fas fa-circle'}, null)
+          ])
+        ]),
       ]),
     ]),
     document.getElementById('root')
