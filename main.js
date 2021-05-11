dc003e1201e021b3a408630be4ab3150c3d3ae8f
Anastasia Starodubtseva <anastasiast1605@gmail.com>
2021-05-11 20:41:43 +0300

    Add swiping option for the mobile devices

--- main.js
+++ main.js
@@ -17,14 +17,8 @@ function reducer(model, action) {
         model.currentIndex +=1;
       }
       return model;
-    case 'FIRST-IMG-INDICATOR':
-      model.currentIndex = 0;
-      return model;
-    case 'SECOND-IMG-INDICATOR':
-      model.currentIndex = 1;
-      return model;
-    case 'THIRD-IMG-INDICATOR':
-      model.currentIndex = 2;
+    case 'SET-TOUCH-DOWN':
+      model.xDown = action.x;
       return model;
     default:
       return model;
@@ -34,7 +28,27 @@ function reducer(model, action) {
 var store = Redux.createStore(reducer, {
   images: [],
   currentIndex: 0,
+  xDown: null
 })
+
+function handleTouchStart(event) {
+  const firstTouch = event.touches[0];
+  store.dispatch({type: 'SET-TOUCH-DOWN', x: firstTouch.clientX})
+}
+
+function handleTouchMove(event) {
+  if (!store.getState().xDown) {
+    return;
+  }
+  var xUp = event.touches[0].clientX;
+  var xDiff = store.getState().xDown - xUp;
+
+  if (xDiff > 0) {
+    store.dispatch({type: 'NEXT-IMAGE'})
+  } else {
+    store.dispatch({type: 'PREVIOUS-IMAGE'})
+  }
+}
 
 const e = React.createElement;
 
@@ -49,29 +63,32 @@ function render() {
     e(ReactRedux.Provider, {store: store}, [
       e('div', {className: 'carousel-items'}, [
         e('p', {className: 'header'}, 'gallery'),
-        e('div', {className: 'img-section'}, [
-          e('img', {className: 'carousel__photo', src: store.getState().images[store.getState().currentIndex].urls.small}, null),
-          e('button', {className: 'carousel__button--prev', onClick: function(event) {
-            store.dispatch({type: 'PREVIOUS-IMAGE'})
-          }}, [ e('i', {className: 'fas fa-chevron-left'}, null)], null),
-          e('button', {className: 'carousel__button--next', onClick: function(event) {
-            store.dispatch({type: 'NEXT-IMAGE'})
-          }}, [ e('i', {className: 'fas fa-chevron-right'}, null)]),
+        e('div', {className: 'main'}, [
+          e('div', {className: 'img-section'}, [
+            e('img', {className: 'carousel__photo', src: store.getState().images[store.getState().currentIndex].urls.small,
+                onTouchMove: handleTouchMove, onTouchStart: handleTouchStart }, null),
+            e('button', {className: 'carousel__button--prev', onClick: function(event) {
+              store.dispatch({type: 'PREVIOUS-IMAGE'})
+            }}, [ e('i', {className: 'fas fa-chevron-left'}, null)], null),
+            e('button', {className: 'carousel__button--next', onClick: function(event) {
+              store.dispatch({type: 'NEXT-IMAGE'})
+            }}, [ e('i', {className: 'fas fa-chevron-right'}, null)]),
+          ]),
         ]),
         e('p', {className: 'title'}, store.getState().images[store.getState().currentIndex].alt_description),
         e('div', {className: 'bottoms-flipping'}, [
           e('button', {className: 'indicator', onClick: function(event) {
-            store.dispatch({type: 'FIRST-IMG-INDICATOR'})
+            store.dispatch({type: 'INDICATOR'})
           }}, [
             e('i', {className: 'fas fa-circle'}, null)
           ]),
           e('button', {className: 'indicator', onClick: function(event) {
-            store.dispatch({type: 'SECOND-IMG-INDICATOR'})
+            store.dispatch({type: 'INDICATOR'})
           }}, [
             e('i', {className: 'fas fa-circle'}, null)
           ]),
           e('button', {className: 'indicator', onClick: function(event) {
-            store.dispatch({type: 'THIRD-IMG-INDICATOR'})
+            store.dispatch({type: 'INDICATOR'})
           }}, [
             e('i', {className: 'fas fa-circle'}, null)
           ])
@@ -81,6 +98,8 @@ function render() {
     document.getElementById('root')
   )
 }
+
+data();
 
 store.subscribe(function() { render(); });
 render();
